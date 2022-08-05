c6103af4e37146ec497039d8611643353a4abea0
Mauro Leggieri <mxmauro@gmail.com>
2022-08-05 12:07:54 -0300

    Recompiled code

--- dist/index.js
+++ dist/index.js
@@ -50,6 +50,22 @@ function run() {
             if (!token) {
                 throw new Error('GITHUB_TOKEN environment variable not found. pass `GITHUB_TOKEN` as env');
             }
+            // Create the GitHub accessor
+            const octokit = github.getOctokit(token);
+            // Get target owner and repository
+            let { repo, owner } = github.context.repo;
+            const ownerRepo = core.getInput('repo');
+            if (ownerRepo) {
+                const ownerRepoItems = ownerRepo.split('/');
+                if (ownerRepoItems.length != 2) {
+                    throw new Error('the specified `repo` is invalid');
+                }
+                owner = ownerRepoItems[0].trim();
+                repo = ownerRepoItems[1].trim();
+                if (owner.length == 0 || repo.length == 0) {
+                    throw new Error('the specified `repo` is invalid');
+                }
+            }
             // Get tag to delete
             const tagName = core.getInput('tag', { required: true });
             if (!tagName) {
@@ -69,22 +85,6 @@ function run() {
             // Get some other flags
             input = core.getInput('ignore_existing');
             const ignoreExisting = (!input) || isYes(input);
-            // Get target owner and repository
-            let { repo, owner } = github.context.repo;
-            const ownerRepo = core.getInput('repo');
-            if (ownerRepo) {
-                const ownerRepoItems = ownerRepo.split('/');
-                if (ownerRepoItems.length != 2) {
-                    throw new Error('the specified `repo` is invalid');
-                }
-                owner = ownerRepoItems[0].trim();
-                repo = ownerRepoItems[1].trim();
-                if (owner.length == 0 || repo.length == 0) {
-                    throw new Error('the specified `repo` is invalid');
-                }
-            }
-            // Create the GitHub accessor
-            const octokit = github.getOctokit(token);
             // Get commit hash to use if one was specified
             let sha = core.getInput('sha');
             if (sha) {
